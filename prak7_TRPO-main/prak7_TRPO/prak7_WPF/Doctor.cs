8391e82321b2b1e1d6acb8bff60dfd28f9ca7377
koLLi196 <mezinets6@gmail.com>
2025-11-07 14:02:00 +0500

    Add files via upload

---
+++ prak7_TRPO-main/prak7_TRPO/prak7_WPF/Doctor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak7_WPF
+{
+    internal class Doctor : INotifyPropertyChanged
+    {
+        private int _id = 0;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _name = "Железнов";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _lastName = "Евгений";
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _middleName = "Иванович";
+        public string MiddleName
+        {
+            get => _middleName;
+            set
+            {
+                if (_middleName != value)
+                {
+                    _middleName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _specialisation = "Хирург";
+        public string Specialisation
+        {
+            get => _specialisation;
+            set
+            {
+                if (value != _specialisation)
+                {
+                    _specialisation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _password = "Password";
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private string _repeatPassword = "";
+
+        public string RepeatPassword
+        {
+            get => _repeatPassword;
+            set
+            {
+                if (_repeatPassword != value)
+                {
+                    _repeatPassword = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
---
+++ prak7_TRPO-main/prak7_TRPO/prak7_WPF/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Numerics;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace prak7_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        private Doctor doctor = new Doctor();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+            doctor = (Doctor)Resources["CurDoctor"];
+            var options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic)
+            };
+            doctor.Id = rand.Next(10000, 100000);
+            string json = JsonSerializer.Serialize(doctor, options);
+            File.WriteAllText($"D_{doctor.Id}.json", json);
+            MessageBox.Show($"Успешно\t {doctor.Id}");
+            Clipboard.SetText(doctor.Id.ToString());
+
+
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            doctor = (Doctor)Resources["CurDoctor"];
+            string doctorFile = $"D_{doctor.Id}.json";
+            string doctorReadFile = File.ReadAllText(doctorFile);
+            var doctorJson = JsonSerializer.Deserialize<Doctor>(doctorReadFile);
+            if (doctor.Password == doctorJson?.Password)
+            {
+                doctor.Name = doctorJson.Name;
+                doctor.LastName = doctorJson.LastName;
+                doctor.MiddleName = doctorJson.MiddleName;
+                doctor.Specialisation = doctorJson.Specialisation;
+                MessageBox.Show("Все гуд");
+
+            }
+            else
+            {
+                MessageBox.Show("Ytn");
+            }
+        }
+    }
+}
